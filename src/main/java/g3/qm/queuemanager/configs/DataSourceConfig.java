fb9a01146daac086ec6c72e2ff6322c042440492
Nickolay Kondratyev <trase.nick@yandex.ru>
2024-05-08 18:04:07 +0300

    Fix transactions enable

--- src/main/java/g3/qm/queuemanager/configs/DataSourceConfig.java
+++ src/main/java/g3/qm/queuemanager/configs/DataSourceConfig.java
@@ -7,8 +7,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableTransactionManagement
 public class DataSourceConfig {
     @Autowired
     private Environment environment;
