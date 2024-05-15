1f767744b0b04dd7ddde55ab858aa14204ee635b
Nickolay Kondratyev <trase.nick@yandex.ru>
2024-05-15 20:07:48 +0300

    App works stable even if no DB connection

--- src/main/java/g3/qm/queuemanager/timers/DecisionCreatorTimer.java
+++ src/main/java/g3/qm/queuemanager/timers/DecisionCreatorTimer.java
@@ -4,6 +4,8 @@ import g3.qm.queuemanager.dtos.DecisionItem;
 import g3.qm.queuemanager.services.DecisionCreatorService;
 import g3.qm.queuemanager.services.DecisionUpdaterService;
 import g3.qm.queuemanager.services.TimerCreatorService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -17,10 +19,17 @@ public class DecisionCreatorTimer extends TimerTask {
     @Autowired
     private DecisionUpdaterService decisionUpdaterService;
 
+    private final Logger LOGGER = LogManager.getLogger(DecisionCreatorTimer.class);
+
     @Override
     public void run() {
-        List<DecisionItem> decision = decisionCreatorService.createDecision();
-        decisionUpdaterService.updateDecision(decision);
-        timerCreatorService.createDecisionCreatorTimer();
+        try {
+            List<DecisionItem> decision = decisionCreatorService.createDecision();
+            decisionUpdaterService.updateDecision(decision);
+        } catch (Exception ex) {
+            LOGGER.error("Error while update decision: ", ex);
+        } finally {
+            timerCreatorService.createDecisionCreatorTimer();
+        }
     }
 }
